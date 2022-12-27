5867620ada9aa0e076a505d801df4d76a36216c0
bangdtbk <bangdtbk@gmail.com>
2022-12-27 12:05:01 +0700

    add digital clock

--- ex2-idomyself/app.js
+++ ex2-idomyself/app.js
@@ -2,7 +2,11 @@ const hourHand = document.querySelector('.hour-hand');
 const minuteHand = document.querySelector('.minute-hand');
 const secondHand = document.querySelector('.second-hand');
 
-function setDate(){
+
+
+
+
+function setDate() {
     const now = new Date();
 
     seconds = now.getSeconds();
@@ -10,12 +14,39 @@ function setDate(){
     secondHand.style.transform = `rotate(${secondDegree}deg)`;
 
     minutes = now.getMinutes();
-    minuteDegree = minutes * 6 + seconds/10 + 90;
+    minuteDegree = minutes * 6 + seconds / 10 + 90;
     minuteHand.style.transform = `rotate(${minuteDegree}deg)`;
 
     hours = now.getHours();
-    hourDegree = hours * 30 + minutes/2 + 90;
+    hourDegree = hours * 30 + minutes / 2 + 90;
     hourHand.style.transform = `rotate(${hourDegree}deg)`;
 }
-setInterval(setDate,1000);
+
+function getCurrenTime() {
+    const now = new Date();
+    let session = "AM";
+    digiSecond = now.getSeconds();
+    digiMinute = now.getMinutes();
+    digiHour = now.getHours();
+
+    if (digiHour == 12) {
+        digiHour = 0;
+        session = "AM";
+    }
+    if (digiHour > 12) {
+        digiHour = digiHour - 12;
+        session = "PM";
+    }
+    digiHour = (digiHour < 10) ? "0" + digiHour : digiHour;
+    digiMinute = (digiMinute < 10) ? "0" + digiMinute : digiMinute;
+    digiSecond = (digiSecond < 10) ? "0" + digiSecond : digiSecond;
+    let time = digiHour + ":" + digiMinute + ":" + digiSecond + " " + session;
+    document.getElementById("digi-clock").innerText = time;
+    let t = setTimeout(function () {
+        getCurrenTime();
+    }, 1000);
+}
+setInterval(setDate, 1000);
 setDate();
+getCurrenTime();
+
